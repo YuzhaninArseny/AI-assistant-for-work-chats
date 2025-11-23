fd537fd47c8d25b3e0852f2f503f1b152009a223
usiqwerty <benlogin1@protonmail.com>
2025-11-23 12:01:39 +0500

    tests: command service

--- apps/command_service/src/command_service_app/api/routes/summarize.py
+++ apps/command_service/src/command_service_app/api/routes/summarize.py
@@ -1,16 +1,26 @@
 from logging import getLogger, INFO
+from typing import Annotated
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+
+from command_service_app.core.service_client import summarization_client, ServiceClient
 from command_service_app.repositories.db_client import DbClientDep
-from command_service_app.core.service_client import summarization_client
-
 
 router = APIRouter(prefix="/summarize", tags=["summarization"])
 logger = getLogger("summarizer")
 logger.setLevel(INFO)
 
+
+def get_summarization_model():
+    return summarization_client
+
+
+SummarizationModelDep = Annotated[ServiceClient, Depends(get_summarization_model)]
+
+
 @router.get("/")
-async def get_messages(dbclient: DbClientDep, chat_id: int, limit: int | None=None) -> str:
+async def get_messages(dbclient: DbClientDep, model_client: SummarizationModelDep,
+                       chat_id: int, limit: int | None = None) -> str:
     messages = dbclient.get_chat_messages(chat_id, limit)
     logger.info(
         f"Summarizing chat {chat_id} from {messages[0].message_id} to {messages[-1].message_id} ({len(messages)} messages)")
@@ -29,6 +39,6 @@ async def get_messages(dbclient: DbClientDep, chat_id: int, limit: int | None=No
         Сообщения:
             {" ".join([msg.content for msg in messages])}
     """
-    summarize_text = await summarization_client.post("/summarize", json={"prompt": prompt})
+    summarize_text = await model_client.post("/summarize", json={"prompt": prompt})
     logger.info(f"Summary for {chat_id} ready ({len(messages)} messages)")
     return summarize_text
--- tests/test_command_service.py
+++ tests/test_command_service.py
@@ -8,19 +8,28 @@ from sqlmodel import Session, SQLModel, create_engine
 from apps.command_service.src.command_service_app.main import app
 from apps.shared.src.shared.db.database import get_session
 from apps.shared.src.shared.models.messages import Message
+from command_service_app.api.routes.summarize import get_summarization_model
 from command_service_app.repositories.db_client import get_client
 from shared.models.base import Base
 
-get_summarization_model = ...  # импортировать реальную зависимость из кода
+
+class FakeSummarizationModel:
+    async def post(self, endpoint: str, json: dict):
+        assert endpoint == "/summarize"
+        assert json['prompt']
+        return "Эффективная суммаризация для чата"
 
 
-class FakeModel:
-    def summarize(self, chat_id, limit):
-        return f"Эффективная суммаризация для чата {chat_id}, лимит {limit}"
+class FakeDbClient:
+    fill: bool
 
+    def __init__(self, fill: bool):
+        self.fill = fill
 
-class FakeClient:
     def get_chat_messages(self, chat_id: int, limit: int | None = None) -> Iterable[Message]:
+        if not self.fill:
+            return []
+
         return [
             Message(
                 message_id=i,
@@ -35,11 +44,7 @@ class FakeClient:
 
 
 def get_summ_model_override():
-    return FakeModel()
-
-
-def fake_get_client():
-    return FakeClient()
+    return FakeSummarizationModel()
 
 
 @pytest.fixture
@@ -53,7 +58,6 @@ def deps_setup():
             return session
 
     app.dependency_overrides[get_session] = get_session_override
-    app.dependency_overrides[get_client] = fake_get_client
     app.dependency_overrides[get_summarization_model] = get_summ_model_override
     yield
     app.dependency_overrides.clear()
@@ -62,9 +66,14 @@ def deps_setup():
     Base.metadata.drop_all(engine)
 
 
-@pytest.mark.xfail
-def test_summarize(deps_setup):
+@pytest.mark.parametrize("with_messages", [True, False], ids=['With messages', 'Without messages'])
+def test_summarize(deps_setup, with_messages):
     client = TestClient(app)
+
+    def fake_get_db_client():
+        return FakeDbClient(with_messages)
+
+    app.dependency_overrides[get_client] = fake_get_db_client
     chat_id = 1
     limit = None
     response = client.get(
@@ -73,6 +82,11 @@ def test_summarize(deps_setup):
     )
 
     data = response.text
-    print(data)
     assert response.status_code == 200
-    assert data == f"Эффективная суммаризация для чата {chat_id}, лимит {limit}"
+
+    # по дефолту фастапи возвращает JSON,
+    # поэтому мы получаем не просто текст, а JSON строку
+    if with_messages:
+        assert data == '"Эффективная суммаризация для чата"'
+    else:
+        assert data == '"В чате нет активности"'
