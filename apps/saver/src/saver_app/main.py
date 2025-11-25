99e50bb49704c8da8ac572350304c05e7eaaddb4
usiqwerty <benlogin1@protonmail.com>
2025-11-25 18:08:00 +0500

    refactor: simplify saver

--- apps/saver/src/saver_app/main.py
+++ apps/saver/src/saver_app/main.py
@@ -1,10 +1,21 @@
-from fastapi import FastAPI
-import os
-from saver_app.api.routes.messages import router as messages_router
+from logging import getLogger, INFO
+
+from fastapi import FastAPI, status
+
+from saver_app.message_repository import MessagesRepoDep
+from shared.schemas.TelegramApiDtos import TelegramMessage
+
+logger = getLogger("saver")
+logger.setLevel(INFO)
 app = FastAPI()
 
-app.include_router(messages_router)
+
+@app.post("/messages", response_model=TelegramMessage, status_code=status.HTTP_201_CREATED)
+async def receive_message(message: TelegramMessage, repo: MessagesRepoDep):
+    logger.info(f"Saving message {message.message_id}")
+    return await repo.add(message)
+
+
 @app.get("/ping")
 def read_root():
     return {"status": "ok", "message": "App is alive!"}
-
---
+++ apps/saver/src/saver_app/message_repository.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+from typing import Annotated
+
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from shared.db.database import get_session
+from shared.models.messages import Message
+from shared.schemas.TelegramApiDtos import TelegramMessage, TelegramUser, TelegramChat
+
+
+# НАДО добавить общий Класс BaseRepo или абстрактный метод, который будет из таблицы table и по значению индекс_колонки
+# index_column доставать list[TableORM]
+class MessageRepository:
+    def __init__(self, session: AsyncSession):
+        self._session = session
+
+    async def add(self, message: TelegramMessage) -> TelegramMessage:
+        db_message = Message(
+            message_id=message.message_id,
+            user_id=message.from_.id,
+            chat_id=message.chat.id,
+            content=message.text,
+            username=message.from_.username,
+            time_sent=datetime.fromtimestamp(message.date),
+        )
+
+        self._session.add(db_message)
+        await self._session.commit()
+        await self._session.refresh(db_message)
+        return TelegramMessage(
+            message_id=db_message.message_id,
+            chat=TelegramChat(id=db_message.chat_id),
+            date=int(db_message.time_sent.timestamp()),
+            **{
+                "from": TelegramUser(
+                    id=db_message.user_id,
+                    username=db_message.username
+                )
+            }
+        )
+
+
+def __get_repo(session: Annotated[AsyncSession, Depends(get_session)]):
+    return MessageRepository(session)
+
+
+MessagesRepoDep = Annotated[MessageRepository, Depends(__get_repo)]
