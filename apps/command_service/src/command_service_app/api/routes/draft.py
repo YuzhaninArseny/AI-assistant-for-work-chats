c2726f4f00cd1d6c6917de9e3813ef861de2d915
usiqwerty <benlogin1@protonmail.com>
2025-11-26 11:30:22 +0500

    tests: draft generator

--- apps/command_service/src/command_service_app/api/routes/draft.py
+++ apps/command_service/src/command_service_app/api/routes/draft.py
@@ -1,13 +1,23 @@
-from fastapi import APIRouter, status
+from typing import Annotated
+
+from fastapi import APIRouter, status, Depends
 from fastapi.responses import JSONResponse
+
+from command_service_app.core.service_client import response_draft_generator_client, ServiceClient
 from command_service_app.repositories.db_client import DbClientDep
-from command_service_app.core.service_client import response_draft_generator_client
 
 router = APIRouter(prefix="/draft", tags=["draft"])
 
 
+def get_draft_client():
+    return response_draft_generator_client
+
+
+DraftGeneratorClientDep = Annotated[ServiceClient, Depends(get_draft_client)]
+
+
 @router.get('/')
-async def draft(db_client: DbClientDep, chat_id: int):
+async def draft(db_client: DbClientDep, chat_id: int, draft_client: DraftGeneratorClientDep):
     try:
         messages = db_client.get_chat_messages(chat_id)
         if len(messages) == 0:
@@ -26,7 +36,7 @@ async def draft(db_client: DbClientDep, chat_id: int):
                 }
             )
 
-        draft = await response_draft_generator_client.post('/draft', json={'messages': messages})
+        draft = await draft_client.post('/draft', json={'messages': messages})
         return JSONResponse(
             status_code=status.HTTP_200_OK,
             content=draft
---
+++ tests/test_command_service/test_draft.py
@@ -0,0 +1,79 @@
+import datetime
+
+import pytest
+from fastapi.testclient import TestClient
+from sqlmodel import Session, SQLModel, create_engine
+
+from apps.command_service.src.command_service_app.main import app
+from apps.shared.src.shared.models.messages import Message
+from command_service_app.api.routes.draft import get_draft_client
+from command_service_app.repositories.db_client import get_session
+from shared.models.base import Base
+
+
+class FakeDraftClient:
+    async def post(self, url: str, json: dict):
+        assert url in {"/draft", "/draft/"}
+        assert json['messages']
+        for msg in json['messages']:
+            assert msg.get('content')
+
+        # ServiceClient возвращает обычные объекты, а не json-репрезентацию
+        # поэтому это строка без кавычек внутри
+        return 'это фейковый текст драфта'
+
+
+def get_fake_draft_client():
+    return FakeDraftClient()
+
+
+@pytest.fixture
+def deps_setup():
+    engine = create_engine("sqlite:///testing.db", connect_args={"check_same_thread": False})
+    SQLModel.metadata.create_all(engine)
+    Base.metadata.create_all(engine)
+
+    def get_session_override():
+        with Session(engine) as session:
+            return session
+
+    app.dependency_overrides[get_session] = get_session_override
+    app.dependency_overrides[get_draft_client] = get_fake_draft_client
+    yield
+    app.dependency_overrides.clear()
+
+    SQLModel.metadata.drop_all(engine)
+    Base.metadata.drop_all(engine)
+
+
+def add_test_messages(session, chat_id):
+    msg = Message(
+        message_id=1,
+        chat_id=chat_id,
+        time_sent=datetime.datetime(year=2025, month=1, day=1),
+        user_id=987,
+        username="admin",
+        content=f"msg content"
+    )
+    session.add(msg)
+    session.commit()
+
+
+@pytest.mark.parametrize("with_messages", [False, True])
+def test_draft(deps_setup, with_messages):
+    client = TestClient(app)
+    db_session = app.dependency_overrides[get_session]()
+    if with_messages:
+        add_test_messages(db_session, 123)
+
+    response = client.get(
+        "/draft/",
+        params={"chat_id": 123}
+    )
+
+    data = response.json()
+    assert response.status_code == 200
+    if with_messages:
+        assert data == 'это фейковый текст драфта'
+    else:
+        assert data == "Невозможно составить черновик ответа - вопросов не обнаружено"
